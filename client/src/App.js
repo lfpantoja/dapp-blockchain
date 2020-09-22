81b33c59a45cc022e3ccea7b0c25beb8014e8032
lfpantoja <lfpantoja@uce.edu.ec>
2020-09-21 20:32:09 -0500

    Control de red

--- client/src/App.js
+++ client/src/App.js
@@ -57,6 +57,7 @@ class App extends Component {
     this.setState({loadingWeb3: true,loadingContract:true,});
     getWeb3.then(results => {
       let web3 = results.web3;
+      
       if (!web3) {
         return this.setState({
           loadingWeb3: false,
@@ -65,18 +66,36 @@ class App extends Component {
           web3: null
         });
       }
-
+      
       let networkName;
       web3.eth.net.getNetworkType((err, networkId) => {
+        switch (networkId) {
+          case "main":
+            networkName = "Main";
+            break;
+          case "morden":
+            networkName = "Morden";
+            break;
+          case "ropsten":
+            networkName = "Ropsten";
+            break;
+          case "rinkeby":
+            networkName = "Rinkeby";
+            break;
+          case "kovan":
+            networkName = "Kovan";
+            break;
+          default:
+            networkName = "Unknown";
+        }
         
-        networkName = "Unknown";
-
         this.setState({
           loadingWeb3: false,
           web3: web3,
           networkName: networkName
         });
         cb();
+        console.log("Red: ",this.state.networkName);
       });
     }).catch((err) => {
       this.setState({loadingWeb3: false, loadingContract: false,});
@@ -94,7 +113,7 @@ class App extends Component {
     console.log("Cargando Contrato")
     const hashStoreContract = contract(HashStoreContract);
     hashStoreContract.setProvider(this.state.web3.currentProvider);
-
+    
     hashStoreContract.deployed().then((hashStoreContractInstance) => {
       this.setState({hashStoreContractInstance});
       this.setState({loadingContract: false,});
@@ -107,7 +126,7 @@ class App extends Component {
   render() {
     let noNetworkError = (this.state.web3 ?
       <h3 className="no-network">Aplicacion de Prueba</h3> :
-      <h3 className="no-network">Conectar con red Ethereum - Metamask</h3>);
+      <h3 className="no-network">Conectar con red Ethereum localhost:7545 - Metamask</h3>);
 
     return (
       <div className="App h-100">
@@ -115,7 +134,7 @@ class App extends Component {
         <NotificationSystem ref="notificationSystem"/>
 
         <Header web3={this.state.web3} loadingWeb3={this.state.loadingWeb3} networkName={this.state.networkName}/>
-
+        
         <main className="flex-fill flex-shrink-0">
             <Container>
               <div className="content">
