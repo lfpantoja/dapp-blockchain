39b367a88ce4d8946a058dd375262c23e77a2b86
lfpantoja <lfpantoja@uce.edu.ec>
2020-10-08 17:30:00 -0500

    Agregación de boton de reconocimiento

--- client/src/components/FetchForm.js
+++ client/src/components/FetchForm.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 import {Button, Row, Col} from 'react-bootstrap';
 
 var Loader = require('react-loader');
@@ -13,6 +13,7 @@ class FetchForm extends Component {
         
       }
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -42,6 +43,19 @@ class FetchForm extends Component {
           return reject(new Error("Paper no encontrado"));
         }
 
+        var autor = values[0];
+        var cuentaActual = this.props.web3.eth.defaultAccount;
+        this.props.web3.eth.getBalance(cuentaActual).then(
+          bal => {
+              console.log('Balance de cuenta actual AR: ',this.props.web3.utils.fromWei(bal, "ether"));
+          }
+        );
+        this.props.web3.eth.getBalance(autor).then(
+          bal => {
+              console.log('Balance de cuenta del autor AR: ',this.props.web3.utils.fromWei(bal, "ether"));
+          }
+        );
+
         submission.sender = values[0];
         submission.hashContent = values[1];
         submission.timestamp = values[2].toNumber();
@@ -59,6 +73,43 @@ class FetchForm extends Component {
           
           resolve(submission);
         });
+      }).catch((err) => {
+        return reject(err);
+      });
+    });
+  }
+
+  //Fóuncin de reconocimiento
+  handleClick = () => {
+    console.log('Botón reconocimiento');
+    console.log('Paper: ', this.props.match.params.id);
+    
+    return new Promise(async (resolve, reject) => {
+      let submission = {};
+      this.props.hashStoreContractInstance.getPaperByID(this.props.match.params.id).then( async (values) => {
+        if (values[0] === "0x0000000000000000000000000000000000000000") {
+          return reject(new Error("Paper no encontrado"));
+        }
+
+        //declaracion de cuentas y valor a transferir
+        var autor = values[0];
+        var cuentaActual = this.props.web3.eth.defaultAccount;
+        const amountToSend = 0.001;
+        submission.hashId = this.props.match.params.id;
+        console.log('Cuenta Actual: ', cuentaActual);
+        console.log('Autor: ', autor);
+
+        if(autor === cuentaActual){
+          console.log('El mismo autor esta viendo el paper');
+        }
+        else{
+          console.log('Reconocimiento');
+          this.props.web3.eth.personal.unlockAccount(cuentaActual, "fernando");
+          const toAddress = autor; //cuenta a recibir ether
+          const amount = 0.00100000; //Monto a enviar
+          const amountToSend = this.props.web3.utils.toWei(amount.toString(), "ether"); //convertir a wei
+          var send = this.props.web3.eth.sendTransaction({from:cuentaActual,to:toAddress, value:amountToSend});
+        }
       }).catch((err) => {
         return reject(err);
       });
@@ -92,6 +143,10 @@ class FetchForm extends Component {
           <div className="my-3">
           <Button href={"https://ipfs.infura.io:5001/api/v0/cat/"+submission.file} 
           className="mt-3 btn-side btn-read" variant="primary" size="dark">Visualizar</Button>
+          </div>
+          <div className="my-3">
+          <Button onClick={this.handleClick} 
+          className="mt-3 btn-side btn-read" variant="primary" size="dark">Reconocimiento</Button>
           </div>
         </Col>
         </Row>
