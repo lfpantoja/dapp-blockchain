d0046cd4dc04b7c9dfae70f6e1e1d50c110be336
lfpantoja <lfpantoja@uce.edu.ec>
2020-12-14 18:54:09 -0500

    Control de nodo de ganache

--- truffle-config.js
+++ truffle-config.js
@@ -5,6 +5,7 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     development: {
+      //host: "127.0.0.1",
       host: "127.0.0.1",
       port: 7545,
       network_id: "5777",
